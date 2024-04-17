062ba6575e1c5797195492804702d51ee1bac248
Lucas Batista <batistalucasdev@gmail.com>
2024-04-17 16:40:37 -0300

    2024.04.17

---
+++ output/estruturas_condicionais/Q6.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    char x;
+    float num1, num2;
+
+    printf("Digite dois numeros: \n");
+    scanf("%f %f",&num1, &num2);
+    printf("Digite a operacao que deseja realizar: + (soma), - (subtracao), / (divisao) ou * (multiplicacao) ");
+    scanf(" %c",&x);
+    if (x == '+')
+    {
+        printf("A soma dos números é: %0.2f",num1 + num2);
+    }
+    else if (x == '-')
+    {
+        printf("A subtracao dos números é: %0.2f",num1 - num2);
+    }
+    else if (x == '/')
+    {
+        printf("A divisao dos números é: %0.2f",num1 / num2);
+    }
+    else if (x == '*')
+    {
+        printf("A multiplicacao dos números é: %0.2f",num1 * num2);
+    }
+    else
+    {
+        printf("Operador invalido.");
+    }
+    getchar();
+    return 0;
+}
